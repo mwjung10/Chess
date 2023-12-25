ef515cf3b7c1c06bbaaba924106b6c42568a0373
Monika Jung <01187233@pw.edu.pl>
2023-12-25 11:53:09 +0100

    move_piece updated

--- gui.py
+++ gui.py
@@ -11,6 +11,7 @@ class ChessMainWindow(QMainWindow):
 
         self.chess_board = ChessBoard()
         self.current_player = Player.WHITE
+        self.selected_piece = None
         self.update_board_display()
 
         # Connect buttons to the move_piece function
@@ -41,11 +42,23 @@ class ChessMainWindow(QMainWindow):
 
     def move_piece(self, row, col):
         try:
-            self.chess_board.move_piece(0, 0, row, col, self.current_player)
-            self.update_board_display()
-            self.switch_turn()
+            if self.selected_piece is None:
+                # If no piece is selected, try to select a piece
+                piece = self.chess_board.pieces[row][col]
+                if piece is not None and piece.player == self.current_player:
+                    self.selected_piece = (row, col)
+                    self.highlight_possible_moves(row, col)
+            else:
+                # If a piece is selected, try to move the piece to the clicked square
+                self.chess_board.move_piece(self.selected_piece[0], self.selected_piece[1], row, col, self.current_player)
+                self.update_board_display()
+                self.switch_turn()
+                self.selected_piece = None
         except (InvalidMove, CoordinatesOutOfRange) as e:
-            print(f"Error: {e}")  #WILL BE CHANGED - PRINT IS ONLY TEMPORARY (to show problems in my code)
+            print(f"Error: {e}")  # TO BE CHANGED
+
+    def highlight_possible_moves(self, row, col):
+        pass
 
     def switch_turn(self):
         self.current_player = Player.BLACK if self.current_player == Player.WHITE else Player.WHITE
